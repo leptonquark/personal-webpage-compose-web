ee445ad08b6a0f69ec2d30882e169e2c87efa5cb
Justin Saler <justin.saler.r@gmail.com>
2023-09-24 14:08:15 +0200

    Add wasm

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
 plugins {
-    kotlin("multiplatform") version "1.9.0"
+    alias(libs.plugins.compose)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.ksp)
     application
 }
 
@@ -7,12 +12,16 @@ group = "me.leptonquark"
 version = "1.0"
 
 repositories {
+    google()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
 }
 
 kotlin {
     js {
+        moduleName = "personal-webpage"
         binaries.executable()
         browser {
             commonWebpackConfig {
@@ -23,15 +32,65 @@ kotlin {
             }
         }
     }
+    @OptIn(org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl::class)
+    wasm {
+        moduleName = "personal-webpage"
+        browser {
+            commonWebpackConfig {
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).copy(
+                    static = (devServer?.static ?: mutableListOf()).apply {
+                        add(project.rootDir.path)
+                    },
+                )
+            }
+        }
+        binaries.executable()
+    }
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.ui)
+                implementation(compose.foundation)
+                implementation(compose.material3)
+                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                implementation(compose.components.resources)
+            }
+        }
         val jsMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-react:18.2.0-pre.346")
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:18.2.0-pre.346")
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.9.3-pre.346")
+                implementation(libs.inject.runtime)
+                kotlin.srcDir("build/generated/ksp/js/jsMain/kotlin")
+
+            }
+        }
+        val wasmMain by getting {
+            dependencies {
+                implementation(libs.serialization.wasm)
             }
         }
     }
 }
+
+dependencies {
+    add("kspJs", libs.inject.compiler)
+}
+
+
+compose {
+    experimental {
+        web.application
+    }
+    kotlinCompilerPlugin.set(libs.versions.compose.get())
+    kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=${libs.versions.kotlin.get()}")
+}
+
+detekt {
+    config.setFrom("detekt-config.yml")
+    source.setFrom("src/commonMain/kotlin", "src/wasmMain/kotlin")
+}
+
 
