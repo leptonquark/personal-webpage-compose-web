a61b14a7cbffd5f641a328c0c40041b449302d07
Justin Saler <justin.saler.r@gmail.com>
2023-09-26 04:03:34 +0200

    Basic startview

--- src/commonMain/kotlin/start/StartView.kt
+++ src/commonMain/kotlin/start/StartView.kt
@@ -1,13 +1,63 @@
 package start
 
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 import theme.AppTheme
+import theme.IconSize
+import theme.Spacing
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 internal fun StartView() {
     AppTheme {
-        Text("Test")
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(Spacing.S),
+                ) {
+                Image(
+                    modifier = Modifier.size(256.dp).clip(CircleShape),
+                    painter = painterResource("images/profile.png"),
+                    contentDescription = "Justin Salér",
+                )
+                Text(
+                    "Justin Salér",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.headlineMedium,
+                )
+                Row(horizontalArrangement = Arrangement.spacedBy(Spacing.M)){
+                    ContactMeIcon("images/github.png")
+                    ContactMeIcon("images/linkedin.png")
+                    ContactMeIcon("images/twitter.png")
+                }
+
+            }
+        }
     }
+}
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+private fun ContactMeIcon(icon: String) {
+    Image(
+        modifier = Modifier.size(IconSize.L),
+        painter = painterResource(icon),
+        contentDescription = null,
+    )
 }
---
+++ src/commonMain/kotlin/theme/IconSize.kt
@@ -0,0 +1,9 @@
+package theme
+
+import androidx.compose.ui.unit.dp
+
+object IconSize {
+    val M = 32.dp
+    val L = 48.dp
+}
+
