675ae2e7407028f3d8ade61977421663fa5a7100
Justin Saler <justin.saler.r@gmail.com>
2023-09-26 08:49:08 +0200

    Update Color

--- src/commonMain/kotlin/start/StartView.kt
+++ src/commonMain/kotlin/start/StartView.kt
@@ -1,6 +1,7 @@
 package start
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,43 +9,64 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import theme.AppTheme
 import theme.IconSize
+import theme.ImageSize
 import theme.Spacing
+
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 internal fun StartView() {
     AppTheme {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.surface),
+            contentAlignment = Alignment.Center,
+        ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(Spacing.S),
+                verticalArrangement = Arrangement.spacedBy(Spacing.M),
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(Spacing.XS),
                 ) {
-                Image(
-                    modifier = Modifier.size(256.dp).clip(CircleShape),
-                    painter = painterResource("images/profile.png"),
-                    contentDescription = "Justin Salér",
-                )
-                Text(
-                    "Justin Salér",
-                    textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.headlineMedium,
-                )
-                Row(horizontalArrangement = Arrangement.spacedBy(Spacing.M)){
-                    ContactMeIcon("images/github.png")
-                    ContactMeIcon("images/linkedin.png")
-                    ContactMeIcon("images/twitter.png")
+                    Image(
+                        modifier = Modifier.size(ImageSize.Profile).clip(CircleShape),
+                        painter = painterResource("images/profile.png"),
+                        contentDescription = "Justin Salér",
+                    )
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(
+                            "Justin Salér",
+                            style = MaterialTheme.typography.headlineMedium,
+                            color = MaterialTheme.colorScheme.onSurface,
+                        )
+                        Text(
+                            "justin.saler.r@gmail.com",
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.titleSmall,
+                            color = MaterialTheme.colorScheme.primary,
+                        )
+                    }
+                }
+                Row(horizontalArrangement = Arrangement.spacedBy(Spacing.S)) {
+                    ContactMeIcon("images/github.png", "@leptonquark")
+                    ContactMeIcon("images/linkedin.png", "@justinsaler")
+                    ContactMeIcon("images/twitter.png", "@leetkingen")
                 }
 
             }
@@ -54,10 +76,18 @@ internal fun StartView() {
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
-private fun ContactMeIcon(icon: String) {
-    Image(
-        modifier = Modifier.size(IconSize.L),
-        painter = painterResource(icon),
-        contentDescription = null,
-    )
+private fun ContactMeIcon(icon: String, profileName: String) {
+    val contentColor = MaterialTheme.colorScheme.secondary
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(Spacing.XXS),
+    ) {
+        Image(
+            modifier = Modifier.size(IconSize.L),
+            painter = painterResource(icon),
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(contentColor, BlendMode.SrcAtop),
+        )
+        Text(text = profileName, color = contentColor, style = MaterialTheme.typography.labelSmall)
+    }
 }
---
+++ src/commonMain/kotlin/theme/ImageSize.kt
@@ -0,0 +1,8 @@
+package theme
+
+import androidx.compose.ui.unit.dp
+
+object ImageSize {
+    val Profile = 256.dp
+}
+
