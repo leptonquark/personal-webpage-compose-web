d40c518297be30eb9980e524c7399433dacdb2b1
Justin Saler <justin.saler.r@gmail.com>
2023-09-29 15:57:00 +0200

    Read JSON through KTOR Client and Kotlin Serializer

--- src/jsMain/kotlin/start/StartViewModel.kt
+++ src/jsMain/kotlin/start/StartViewModel.kt
@@ -1,12 +1,14 @@
 package start
 
+import config.Config
 import config.ConfigRepository
 import di.Singleton
 import file.FileDownloadHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.stateIn
 import me.tatarka.inject.annotations.Inject
 import start.contactme.ContactMeItem
@@ -26,30 +28,54 @@ sealed interface StartIntent {
 
 private const val RESUME_URL = "/resume.pdf"
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @Singleton
 class StartViewModel @Inject constructor(
-    private val configRepository: ConfigRepository,
+    configRepository: ConfigRepository,
     private val externalUrlHandler: ExternalUrlHandler,
     private val fileDownloadHandler: FileDownloadHandler,
 ) {
     private val viewModelScope = CoroutineScope(Dispatchers.Main)
 
-    val state = flow {
-        emit(
-            StartState(
-                name = configRepository.name,
-                emailAddress = configRepository.email,
-                contactMeItems = configRepository.contactMeItems,
-            )
-        )
+    val state = configRepository.config.mapLatest { config ->
+        config.getStartState()
     }.stateIn(
         scope = viewModelScope,
         started = SharingStarted.Eagerly,
         initialValue = StartState()
     )
 
-    fun sendIntent(intent: StartIntent) = when(intent){
+    fun sendIntent(intent: StartIntent) = when (intent) {
         is StartIntent.ContactMeItemClick -> externalUrlHandler.navigateTo(intent.item.url)
         StartIntent.DownloadResumeClick -> fileDownloadHandler.download(RESUME_URL)
     }
+
+    private fun Config.getStartState() = StartState(
+        name = name,
+        emailAddress = email,
+        contactMeItems = contactMe.mapNotNull { url -> getContactMeItemOrNull(url) }.toSet(),
+        )
+
+    private fun getContactMeItemOrNull(url: String) : ContactMeItem? {
+        val patterns = mapOf(
+            "LinkedIn" to Regex("https://www\\.linkedin\\.com/in/([^/]+)"),
+            "GitHub" to Regex("https://github\\.com/([^/]+)"),
+            "Twitter" to Regex("https://twitter\\.com/([^/]+)")
+        )
+
+        val (platform, userName) = patterns.entries.firstNotNullOfOrNull { (platform, pattern) ->
+            pattern.find(url)?.run { platform to groupValues[1] }
+        } ?: (null to null)
+
+        return userName?.let {
+            when (platform) {
+                "GitHub" -> ContactMeItem.GitHub(it)
+                "LinkedIn" -> ContactMeItem.LinkedIn(it)
+                "Twitter" -> ContactMeItem.Twitter(it)
+                else -> null
+            }
+        }
+    }
 }
+
+
