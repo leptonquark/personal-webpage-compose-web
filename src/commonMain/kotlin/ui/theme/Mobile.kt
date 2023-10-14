c916ccbc372a9917d9e85b47c19f2f2154d6b7d5
Justin Saler <justin.saler.r@gmail.com>
2023-10-14 23:28:17 +0200

    Reduce duplicate code

--- src/commonMain/kotlin/ui/theme/Mobile.kt
+++ src/commonMain/kotlin/ui/theme/Mobile.kt
@@ -1,3 +1,9 @@
 package ui.theme
 
-expect val isMobile: Boolean
+expect val userAgent: String
+
+val isMobile: Boolean
+    get()  {
+        val mobileKeywords = setOf("Mobile", "Android", "iPhone", "iPad", "iPod", "Windows Phone")
+        return mobileKeywords.any { keyword -> userAgent.contains(keyword) }
+    }
--- src/wasmJsMain/kotlin/ui/theme/Mobile.wasmJs.kt
+++ src/wasmJsMain/kotlin/ui/theme/Mobile.wasmJs.kt
@@ -2,10 +2,4 @@ package ui.theme
 
 import kotlinx.browser.window
 
-actual val isMobile: Boolean
-    get()  {
-    val userAgent = window.navigator.userAgent
-    val mobileKeywords = setOf("Mobile", "Android", "iPhone", "iPad", "iPod", "Windows Phone")
-
-    return mobileKeywords.any { keyword -> userAgent.contains(keyword) }
-}
+actual val userAgent: String get() = window.navigator.userAgent
