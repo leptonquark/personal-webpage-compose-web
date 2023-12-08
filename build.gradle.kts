5978b31254acb0909869d6fedb4bde79e7e02451
Justin Saler <63164104+leptonquark@users.noreply.github.com>
2023-12-08 12:53:46 +0100

    Migrate to Compose Beta! (#25)

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,6 @@ repositories {
     google()
     gradlePluginPortal()
     mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
     maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
@@ -88,8 +86,6 @@ compose {
     experimental {
         web.application
     }
-    kotlinCompilerPlugin.set(libs.versions.composecompiler.get())
-    kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=${libs.versions.kotlin.get()}")
 }
 
 detekt {
