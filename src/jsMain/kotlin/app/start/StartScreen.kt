ee8c5834b598aff9885f2777a1342848c5b35261
Justin Saler <63164104+leptonquark@users.noreply.github.com>
2023-12-30 17:53:45 +0800

    Add a loading spinner (#37)
    * Colored background
    
    * Center spinner
    
    * Hide the loader onWasmReady
    
    * set loader color to theme color
    
    * Fix detekt

--- src/jsMain/kotlin/app/start/StartScreen.kt
+++ src/jsMain/kotlin/app/start/StartScreen.kt
@@ -4,17 +4,24 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
+import kotlinx.browser.document
 import org.jetbrains.skiko.wasm.onWasmReady
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import org.w3c.dom.Document
+
+private const val LOADER_ID = "loader"
 
 @OptIn(ExperimentalComposeUiApi::class)
-class StartScreen: KoinComponent {
+class StartScreen : KoinComponent {
 
-    private val viewModel : StartViewModel by inject()
+    private val viewModel: StartViewModel by inject()
+
+    private val Document.loader get() = getElementById(LOADER_ID)
 
     fun initialize() {
         onWasmReady {
+            document.loader?.remove()
             CanvasBasedWindow("Justin Salér") {
                 val state by viewModel.state.collectAsState()
                 StartView(
